552c57d1f3029d861eb5d6552c75cf561181224d
qubies <tobias@renwick.tech>
2018-01-13 14:11:53 -0700

    initial

---
+++ crypto.go
@@ -0,0 +1,57 @@
+package crypto
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+func pad(blockSize int, input *[]byte) error {
+	//the padding is the length of padding added
+	iLen := len(*input)
+	//so the blocksize - the leftover is the amount we have to add
+	//if the blocksize fits perfectly, we add a full block of padding ie 0x32 * 32
+	pLen := blockSize - (iLen % blockSize)
+	//build the padding text
+	padding := bytes.Repeat([]byte{byte(pLen)}, pLen)
+	//add it to the initial string
+	*input = append(*input, padding...)
+	return nil
+}
+func encrypt(key, plain []byte) {
+	key = []byte("example key 1234")
+	plain = []byte("exampleplaintext")
+
+	// CBC mode works on blocks so plaintexts may need to be padded to the
+	// next whole block. For an example of such padding, see
+	// https://tools.ietf.org/html/rfc5246#section-6.2.3.2. Here we'll
+	// assume that the plaintext is already of the correct length.
+	if len(plain)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plain))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plain)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+
+	fmt.Printf("%x\n", ciphertext)
+}
---
+++ crypto_test.go
@@ -0,0 +1,39 @@
+package crypto
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestPadding(t *testing.T) {
+	thingToPad := []byte("IAMWRONGLENGTH")
+	//copy the slice
+	initialPad := append([]byte(nil), thingToPad...)
+	blockSize := 32
+	pad(blockSize, &thingToPad)
+	if len(thingToPad) != blockSize {
+		t.Errorf("Byte Length Incorrect after padding\nString:%s\nHex:%x", thingToPad, thingToPad)
+
+	}
+	//create a block of text that is all the blocksize chars.
+	trickyPad := bytes.Repeat([]byte{byte(blockSize)}, blockSize)
+	initialTricky := append([]byte(nil), trickyPad...)
+	pad(blockSize, &trickyPad)
+	if len(trickyPad) != blockSize*2 {
+		t.Errorf("Byte Length Incorrect on Tricky Pad\nString:%s\nHex:%x", thingToPad, thingToPad)
+
+	}
+	if !bytes.Equal(trickyPad, initialTricky) {
+		t.Errorf("Unpad Failed on Tricky Pad\nString:%s\nHex:%x", trickyPad, trickyPad)
+	}
+
+	if !bytes.Equal(thingToPad, initialPad) {
+		t.Errorf("Unpad Failed on Inital Pad Pad\nString:%s\nHex:%x", thingToPad, thingToPad)
+
+	}
+
+}
+
+func TestCrypto(t *testing.T) {
+	t.Error("Not Yet Implemented")
+}
